d2527c20e7665df135c1b14b3b73d660cf5260a8
Mustafa Ali <mustafaali8254@gmail.com>
2024-02-23 16:21:15 +0500

    File Updated

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// alert("Hello")
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+});
